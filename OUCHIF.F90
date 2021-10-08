c15d41ca2781de04f29f3ef494afb7bf2588b387
Owain Kenway <o.kenway@ucl.ac.uk>
2021-10-08 14:36:38 +0100

    Fix Intel Fortran RNG.

---
+++ OUCHIF.F90
@@ -0,0 +1,65 @@
+! Code to abstract GFortran specific calls.
+! Owain Kenway
+
+! Equivalent of iargc
+subroutine countargs(n)
+  use ISO_FORTRAN_ENV
+  implicit none
+
+  integer, intent(out) :: n
+
+  n = command_argument_count()
+end
+
+! Equivalent of getarg
+subroutine getargat(n, s, l)
+  use ISO_FORTRAN_ENV
+  implicit none
+
+  integer, intent(in) :: n
+  integer, intent(out) :: l
+  character (len=1024), intent(out) :: s
+
+  call get_command_argument(n, length=l)
+  call get_command_argument(n, s)
+end
+
+! Get time in seconds
+subroutine getsec(t)
+  use ISO_FORTRAN_ENV
+  implicit none
+
+  real, intent(out) :: t
+
+  call cpu_time(t)
+end
+
+! Start random number generation.
+! Technically we don't need to do anything in OpenWatcom
+subroutine initrand(s)
+  use ifport
+  implicit none
+
+  integer, intent(in) :: s
+
+  call seed(s)
+end
+
+! Get next number.
+! Need to pass seed as that's always needed by OpenWatcom
+function getrand(s)
+  use ifport
+  implicit none
+
+  real :: getrand
+  integer :: s
+
+  getrand = rand()
+
+end
+
+! Sleep that only happens on DOS
+  subroutine dossleep(seconds)
+    implicit none
+    integer :: seconds
+  end
